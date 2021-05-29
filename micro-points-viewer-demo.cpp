8676574944f7d8b5f0344f4425485ab7d6a1fbc8
SGrottel <webmaster@sgrottel.de>
2021-05-29 07:01:09 +0200

    Initial commit of demo application and ci configuration

---
+++ micro-points-viewer-demo.cpp
@@ -0,0 +1,19 @@
+﻿// micro-points-viewer-cmake.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello\n";
+#if defined(DEBUG) || defined(_DEBUG)
+	std::cout << "I am a Debug build.\n";
+#else
+	std::cout << "I am a Release build.\n";
+#endif
+	std::cout << "I am " << 8 * sizeof(void*) << " bit wide.\n";
+
+	std::cout << std::endl;
+	return 0;
+}
